f9c4a8f0df40c6fb83400ee999925197394ede97
Akira Valade <88116996+Akkibi@users.noreply.github.com>
2025-10-23 23:47:47 +0200

    fix for deploy, fix collision for jump

--- src/components/multBanner.tsx
+++ src/components/multBanner.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 
 const MultBanner = ({ caffeineLvl }: { caffeineLvl: number }) => {
   // const ref = useRef<HTMLDivElement>(null);
