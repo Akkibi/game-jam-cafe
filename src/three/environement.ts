5755cf8e4c7503a223a5f5b52ce799edf7a6492e
Fabrizio Manetti <fmanetti@student.42.fr>
2025-10-24 16:00:55 +0200

    Added elevator

--- src/three/environement.ts
+++ src/three/environement.ts
@@ -15,6 +15,7 @@ export class Environement {
 		this.physicsEngine = PhysicsEngine.getInstance();
 		loadGLTFModel(this.instanceGroup, "/assets/bounds/bounds.glb");
 
+		this.createElevatorPlane();
 		const wall1Position = new THREE.Vector3(-3.4, 0, 0);
 		const wall1Scale = new THREE.Vector3(0.1, 10, 0);
 		const wall2Position = new THREE.Vector3(3.4, 0, 0);
@@ -27,6 +28,31 @@ export class Environement {
 		const boxPosition = new THREE.Vector3(0, -1, 0);
 		const boxScale = new THREE.Vector3(0.1, 0.1, 0.1);
 		this.physicsEngine.addObject(boxPosition, boxScale, 0, true);
+	}
+
+	private createElevatorPlane() {
+		// Create a plane geometry for the elevator effect
+		const geometry = new THREE.PlaneGeometry(0.8, 0.8);
+
+		// Load texture for the elevator frames
+		const textureLoader = new THREE.TextureLoader();
+		const texture = textureLoader.load("/assets/elevator.png");
+
+		// Create a material with transparency for elevator effect
+		const material = new THREE.MeshBasicMaterial({
+			map: texture,
+			// color: "red",
+			transparent: true,
+			side: THREE.DoubleSide,
+		});
+
+		const mesh = new THREE.Mesh(geometry, material);
+
+		// Position the plane under the steam wand
+		mesh.position.set(-3.38, 2, -0.3);
+		mesh.rotation.set(0, Math.PI * 0.5, 0);
+
+		this.scene.add(mesh);
 	}
 
 	public static getInstance(scene: THREE.Scene): Environement {
